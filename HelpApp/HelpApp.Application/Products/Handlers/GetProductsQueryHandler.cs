af619ea1d1359e6cb0f68e6fa4a53686c0ca4256
rafaelromwno <romanosilvarafael@gmail.com>
2025-05-26 12:41:53 -0300

    Tratamento de Handlers com uso de commands de Produto na camada de aplicação

---
+++ HelpApp/HelpApp.Application/Products/Handlers/GetProductsQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using HelpApp.Application.Products.Queries;
+using HelpApp.Domain.Entities;
+using HelpApp.Domain.Interfaces;
+using MediatR;
+
+namespace HelpApp.Application.Products.Handlers;
+
+public class GetProductsQueryHandler : IRequestHandler<GetProductsQuery, IEnumerable<Product>>
+{
+    #region Atributos
+
+    private readonly IProductRepository _productRepository;
+
+    #endregion
+
+    #region Construtor
+
+    public GetProductsQueryHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    #endregion
+
+    public async Task<IEnumerable<Product>> Handle(GetProductsQuery resquest,
+        CancellationToken cancellationToken)
+    {
+        return await _productRepository.GetProducts();
+    }
+}
+
